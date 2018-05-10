2d3bb620e890ffcd8ba68620189bdec1f98e1fa9
singh-anita <anita.singh@built.io>
2018-05-10 14:36:06 +0530

    Title updated

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,110 +1,112 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Panel, Button, FormGroup, FormControl, ControlLabel,Checkbox } from "react-bootstrap";
+import { ListGroup,ListGroupItem, Button, FormGroup, FormControl, ControlLabel, Checkbox } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import './dashboard.css';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            list: [
+                
+            ], value: ''
+        };
+
+    // this.handleChange = this.handleChange.bind(this);
+        /*  this.handleSubmit = this.handleSubmit.bind(this);*/
+    }
+    add() {
+        //  alert(e.target.value)
+        this.setState({
+           list: this.state.list.concat([{ pro_name: this.state.value }]),
+            value: ''
+        });
+    }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    /*handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }*/
+    /*  state = {
+          add : null
+      }
+      addTodo = () =>
+            {
+          this.setState({ add : <ListGroupItem bsStyle="success">Success</ListGroupItem> })
+      }*/
+    update(e) {
+        // alert(e.target.value)
+        this.setState({
+            value: e.target.value
+        })
+    }
+
     render() {
-        var material = {
-            marginRight: 10
+
+        var edit = {
+            marginRight: '15px',
+            marginTop: '3px'
+        }
+        var delet = {
+            marginTop: '3px'
+        }
+        var content = {
+            marginBottom: '10px'
         }
         return (
             <div class="container">
                 <header class="page-title">
-                    <h1><i class="material-icons" style={material}>view_list</i>TODO</h1>
-
-                    <div class="tag"> An easy way to manage your work</div>
                 </header>
-                <div class="addlist col-md-6">
-                    <input type="text" class="form-control add-todo" placeholder="Add todo" />
-                </div>
-                <div class="cont_add_titulo_cont"><a href="#e" onclick="add_new()"><i class="material-icons"></i></a>
-                </div>
                 <div class="todo-in-progress">
                     <h2> Working tasks</h2>
                     <div class="left_menu">
-                        <ListGroup>
-                            <ListGroupItem bsStyle="success">Success</ListGroupItem>
-                            <ListGroupItem bsStyle="info">Info</ListGroupItem>
-                            <ListGroupItem bsStyle="warning">Warning</ListGroupItem>
-                            <ListGroupItem bsStyle="danger">Danger</ListGroupItem>
-                        </ListGroup>
+                        <div class="cont">
+                            <h3 style={{ marginLeft: '6px', paddingTop: '3px' }}>
+                                TO DO
+                            </h3>
+                            <div class="addlist">
+                                <input type="text" onChange={(e) => { this.update(e) }} class="form-control add-todo" placeholder="Add todo" value={this.state.value} />
+                            </div>
+                            <Button onClick={() => { this.add() }}>
+                                <span>Add Project</span>
+                            </Button>
+                        </div>
+                           <ListGroup>
+                            {
+                                this.state.list.map((curr, index) => {
+                                        return (
+                                            <ListGroupItem>{curr.pro_name}</ListGroupItem>
+                                        );
+                                })
+                            }   
+                            </ListGroup>       
                     </div>
                     <div class="content_container">
-                        <Panel bsStyle="primary">
-                            <Panel.Heading>
-                                <Panel.Title componentClass="h3">Panel heading<i class="material-icons md-36-title icon-delete">delete</i></Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                            <div class="description">
-                                 <Checkbox>Panel content</Checkbox>
-                                </div>
-                                <div class="action"> <i class="material-icons md-36-con icon-edit">mode_edit</i>
-                                <i class="material-icons md-36-con icon-delete">delete</i>
-                                </div>
-                           
-                            </Panel.Body>
-                        </Panel>
+                        <Button style={content}  >
+                            <span class="glyphicon glyphicon-plus adder_icon" style={{ marginRight: '10px' }}></span>
+                            Add Item
+                    </Button>
 
-                        <Panel bsStyle="success">
-                            <Panel.Heading>
-                                <Panel.Title componentClass="h3">Panel heading<i class="material-icons md-36-title icon-delete">delete</i></Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                            <div class="description">
-                                 <Checkbox>Panel content</Checkbox>
+                        <ListGroup componentClass="ul">
+                            <ListGroupItem>
+                                <div class="description">
+                                    <Checkbox>Item 1</Checkbox>
                                 </div>
                                 <div class="action">
-                                <i class="material-icons md-36-con">mode_edit</i>
-                                <i class="material-icons md-36-con icon-delete">delete</i>
+                                    <Button style={edit}>
+                                        <i class="glyphicon glyphicon-pencil"></i>
+                                    </Button>
+                                    <Button style={delet}>
+                                        <i class="glyphicon glyphicon-trash"></i>
+                                    </Button>
                                 </div>
-                                </Panel.Body>
-                        </Panel>
+                            </ListGroupItem>
 
-                        <Panel bsStyle="info">
-                            <Panel.Heading>
-                                <Panel.Title componentClass="h3">Panel heading<i class="material-icons md-36-title icon-delete">delete</i></Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                            <div class="description">
-                                 <Checkbox>Panel content</Checkbox>
-                                </div>
-                            <div class="action">
-                            <i class="material-icons md-36-con icon-edit">mode_edit</i>
-                                <i class="material-icons md-36-con icon-delete">delete</i>
-                                </div>
-                                </Panel.Body>
-                        </Panel>
-
-                        <Panel bsStyle="warning">
-                            <Panel.Heading>
-                                <Panel.Title componentClass="h3">Panel heading<i class="material-icons md-36-title icon-delete">delete</i></Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                            <div class="description">
-                                 <Checkbox>Panel content</Checkbox>
-                                </div>
-                            <div class="action">
-                                <i class="material-icons md-36-con">mode_edit</i>
-                                <i class="material-icons md-36-con icon-delete">delete</i>
-                                </div>
-                                </Panel.Body>
-                        </Panel>
-
-                        <Panel bsStyle="danger">
-                            <Panel.Heading>
-                                <Panel.Title componentClass="h3">Panel heading<i class="material-icons md-36-title icon-delete">delete</i></Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body>
-                            <div class="description">
-                                 <Checkbox>Panel content</Checkbox>
-                                </div>
-                            <div class="action">
-                                <i class="material-icons md-36-con">mode_edit</i>
-                                <i class="material-icons md-36-con icon-delete">delete</i>
-                            </div>
-                                </Panel.Body>
-                        </Panel>
+                        </ListGroup>
                     </div>
                 </div>
             </div>
