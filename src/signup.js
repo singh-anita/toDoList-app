904729e84ec126d1f22348ba68098510c6de0080
singh-anita <anita.singh@built.io>
2018-06-13 20:38:00 +0530

    working on validation client

--- src/signup.js
+++ src/signup.js
@@ -27,142 +27,116 @@ class Signup extends Component {
                 "Authorization": localStorage.getItem('authtoken')
             }
         }).then(
-             (response) => {
+            (response) => {
                 if (response.status == 200) {
                     this.setState({ redirect: true })
                 }
             }
         )
     }
-    //  isLoading: false,  newUser: null
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            email: "",
-            username: "",
-            password: "",
-            confpsswd: "",
-            redirect: false,
-            emailmsg:'',
-            usermsg:'',
-            passwordmsg:'',
-            confirmsg:''
-           // isDisabled:false
-            // errors: []
-            // touched: {
-            //     email: false,
-            //     username:false,
-            //     password: false,
-            //     confpsswd: false
-            //   },
-        };
-        
-        this.handleChange = this.handleChange.bind(this);
-       // this.validate=this.validate.bind(this);
+
+    state = {
+        email: '',
+        username : '',
+        password : '',
+        confirmPassword : '',
+
+
+        isInvalidEmail: true,
+        usernameLengthZero : true,
+        passwordLengthZero : true,
+        isPasswordMatching : false,
+
+        canBeSubmitted : false,
+    };
+
+    handleEmailChange(event) {
+        this.setState({ email: event.target.value },
+            () => {
+                console.log("IN HERE")
+                this.validateEmail();
+            })
     }
 
-    //   formIsValid() {
-    //     var state = this.state;
-
-    //     if (!validator.isEmail(state.email.value)) {
-    //     //   state.email.isValid = false;
-    //     //   state.email.message = 'Not a valid email address';
-
-    //       this.setState(state);
-    //       return false;
-    //     }
-
-    //     //additional validation checks here...
-
-    //     return true;
-    //   }
-
-   validateForm(email,username,password,confpsswd) {
-    let formIsValid = true;
-   /* var x = 
-    (
-        this.state.email.length > 0 &&
-        this.state.username.length > 0 &&
-        this.state.password.length > 0 &&
-        (this.state.password === this.state.confpsswd)
-     );
-     
-     console.log("RUNNING..... : ", x)
-     return x*/
-     if (!validator.isEmail(email)) {
-       // var state = this.state;
-       formIsValid =false;
-       // state.email.isValid = false;
-       email = 'Not a valid email address';
-        
-       // this.setState(state);
-       // return false;
-      }
-      if(!validator.isAlpha(username)){
-       // var state = this.state;
-        formIsValid =false;
-        username='Username must be in alphabets only';
-         // this.setState(state);
-           //return false;
-      }
-      if(!validator.isAlphanumeric(password)){
-      //  var state = this.state;
-        formIsValid =false;
-        password ='Password must contain alphabets and numbers';
-    // this.setState(state);
-       // return false;
-      }
-      if(!validator.equals(password,confpsswd))
-      {
-        //var state = this.state;
-        formIsValid =false;
-        confpsswd='confirm Password must match with password';
-        //this.setState(state);
-       // return false;
-      }
-
-        return  formIsValid; 
-        //   this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    handleUserNameChange(event){
+        this.setState({ username : event.target.value },
+        () =>{
+            this.isUsernameLengthZero();
+        })
     }
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+    handlePassword(event){
+        this.setState({ password : event.target.value },
+        () =>{
+            if(this.state.password.length === 0){
+                this.setState({ passwordLengthZero : true })
+            }
+            else{
+                this.setState({ passwordLengthZero : false })
+            }
+
+            this.comparePasswords()
+        })
     }
-    handleBlur = (field) => (evt) => {
-        this.setState({
-          touched: { ...this.state.touched, [field]: true },
-        });
-      }
-    
-    renderRedirect() {
-        if(this.state.redirect){
-           // return <Redirect to='/dashboard'/>
-           this.props.history.push('/dashboard')
+
+    handleConfirmPassword(event){
+        this.setState({ confirmPassword : event.target.value },
+        () =>{
+            this.comparePasswords()
+        })
+    }
+
+    isUsernameLengthZero(){
+        if(this.state.username.length === 0){
+            this.setState({ usernameLengthZero : true })
+        }
+        else{
+            this.setState({ usernameLengthZero : false })
         }
     }
-    canBeSubmitted() {
-        const errors = validateForm(this.state.email,this.state.username, this.state.password, this.state.confpsswd);
-        const isEnabled = Object.keys(errors).some(x => errors[x]);
-        return !isEnabled;
-      }
+
+    comparePasswords(){
+        if(validator.equals(this.state.password, this.state.confirmPassword)){
+            this.setState({ isPasswordMatching : true })
+        }
+        else{
+            this.setState({ isPasswordMatching : false })
+        }
+    }
+
+    validateEmail() {
+        if (validator.isEmail(this.state.email)) {
+            this.setState({ isInvalidEmail: false })
+        }
+        else{
+            this.setState({ isInvalidEmail: true })
+        }
+    }
+
+
+    /* WHEN EVERYTHING IS RIGHT, EXECUTE THIS.... */
+    allSet(){
+        ( this.state.isInvalidEmail &&
+          this.state.isPasswordMatching ) ?
+          this.setState({ allSet : true }) :
+          this.setState({ allSet : false })
+    }
+
+    renderRedirect() {
+        if (this.state.redirect) {
+            this.props.history.push('/dashboard')
+        }
+    }
+
     handleSubmit(e) {
-    // const { email, username, password ,confpsswd} = this.state;
-    // const errors = validate(email,username, password,confpsswd);
-    // if (errors.length > 0) {
-    //   this.setState({ errors });
-    //   return;
-    // }
-    // if (!this.validateForm()) {
-    //     e.preventDefault();
-    //     return;
-    //   }
-      if (!this.canBeSubmitted()) {
-        e.preventDefault();
-        return;
-      }
+        
+        if (!this.canBeSubmitted) {
+            e.preventDefault();
+            return;
+        }
+
+
         var obj = {
             email: this.state.email,
             username: this.state.username,
@@ -177,9 +151,9 @@ class Signup extends Component {
         })
             .then((response) => {
                 console.log(response.data.authtoken);
-               // if(response.status == 200){
-              
-              if (!localStorage.getItem('authtoken')) {
+                // if(response.status == 200){
+
+                if (!localStorage.getItem('authtoken')) {
                     //save it in localStorage
                     localStorage.setItem('authtoken', (response.data.authtoken));
                     console.log("Saved in localStorage ");
@@ -187,39 +161,18 @@ class Signup extends Component {
                     this.setState({
                         redirect: true
                     })
-              }
-             /*  else {
-                    if (response.data.redirect == '/')
-                        this.setState({
-                            redirect: true
-                        })
-                 }*/
+                }
             })
             .catch(function (error) {
                 console.log(error.response)
-               // console.log(error);
             });
     }
 
     render() {
-    //     const errors = validate(this.state.email,this.state.username, this.state.password, this.state.confpsswd);
-    //     const isDisabled = Object.keys(errors).some(x => errors[x]);
-    //    // const { errors } = this.state;
-
-    const errors = validateForm(this.state.email,this.state.username, this.state.password, this.state.confpsswd);
-    const isEnabled = Object.keys(errors).some(x => errors[x]);
-    const errorMsg = (field) => {
-        const hasError = errors[field];
-        const shouldShow = this.state.touched[field];
-        
-        return hasError ? shouldShow : false;
-      };
-      
-    //const isEnabled = this.validateForm();
         return (
             <div className="container">
- <Header/>
- 
+                <Header />
+                {/* { this.allSet() } */}
                 <div id="signupbox" style={{ marginTop: 50 }} className="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                     <div className="panel panel-info">
                         <div className="panel-heading">
@@ -233,18 +186,18 @@ class Signup extends Component {
                                         Email
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl name="email" type="email" placeholder="Email" value={this.state.email} onChange={(e) => { this.handleChange(e) }} onBlur={this.handleBlur('email')}/>
-                                        <div className={errorMsg('email') ? "error" : ""}>{this.state.emailmsg}</div>
+                                        <FormControl onChange={this.handleEmailChange.bind(this)} name="email" type="email" placeholder="Email" value={this.state.email} />
+                                        { (this.state.isInvalidEmail) ? <div>Invalid email format!</div> : null}
                                     </Col>
-                                  
+
                                 </FormGroup>
                                 <FormGroup controlId="username">
                                     <Col componentClass={ControlLabel} md={3}>
                                         User Name
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl name="username" type="text" placeholder="Username" value={this.state.username} onChange={(e) => { this.handleChange(e) }} onBlur={this.handleBlur('username')}/>
-                                        <div className={errorMsg('username') ? "error" : ""}>{this.state.usermsg}</div>
+                                        <FormControl onChange = { this.handleUserNameChange.bind(this) } name="username" type="text" placeholder="Username" value={this.state.username} />
+                                        { (this.state.usernameLengthZero) ? <div>Set some username...</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup controlId="password">
@@ -252,8 +205,8 @@ class Signup extends Component {
                                         Password
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl name="password" type="password" placeholder="Password" value={this.state.password} onChange={(e) => { this.handleChange(e) }} onBlur={this.handleBlur('password')}/>
-                                        <div className={errorMsg('password') ? "error" : ""}>{this.state.passwordmsg}</div>
+                                        <FormControl onChange = { this.handlePassword.bind(this) } name="password" type="password" placeholder="Password" value={this.state.password} />
+                                        { (this.state.passwordLengthZero) ? <div>Password cannot be empty...</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup controlId="confpsswd">
@@ -261,23 +214,21 @@ class Signup extends Component {
                                         Confirm Password
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl name="confpsswd" type="password" placeholder="Confirm Password" value={this.state.confpsswd} onChange={(e) => { this.handleChange(e) }} onBlur={this.handleBlur('confpsswd')}/>
-                                        <div className={errorMsg('confpsswd') ? "error" : ""}>{this.state.confirmsg}</div>
+                                        <FormControl onChange = { this.handleConfirmPassword.bind(this) } name="confpsswd" type="password" placeholder="Confirm Password" value={this.state.confpsswd} />
+                                        { (!this.state.isPasswordMatching) ? <div>Passwords donot match!</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup>
                                     <Col smOffset={3} mdOffset={3} sm={9} md={6}>
                                         {this.renderRedirect()}
-                                        <Button type="button" bsStyle="success" onClick={(e) => this.handleSubmit(e)} disabled={!isEnabled}><i className="icon-hand-right"></i>Sign Up</Button>
+                                        <Button type="button" bsStyle="success" disabled = { !this.state.canBeSubmitted } ><i className="icon-hand-right"></i>Sign Up</Button>
                                         <span style={{ marginLeft: 8 }}> OR </span>
                                         <Button type="submit" bsStyle="primary">Sign Up with Stackoverflow</Button>
                                     </Col>
-                                    {/* {errors.map(error => (
-          <p key={error}>Error: {error}</p>
-        ))} */}
+
                                 </FormGroup>
                             </Form>
-                            
+
                         </div>
 
                     </div>
