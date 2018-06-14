4364458dcece462924301b6927b6471de6156f29
singh-anita <anita.singh@built.io>
2018-06-14 17:25:48 +0530

    client side validation properly done

--- src/signup.js
+++ src/signup.js
@@ -38,89 +38,127 @@ class Signup extends Component {
 
     state = {
         email: '',
-        username : '',
-        password : '',
-        confirmPassword : '',
-
+        username: '',
+        password: '',
+        confirmPassword: '',
+        touched: false,
 
         isInvalidEmail: true,
-        usernameLengthZero : true,
-        passwordLengthZero : true,
-        isPasswordMatching : false,
-
-        canBeSubmitted : false,
+        usernameLengthZero: true,
+        passwordLengthZero: true,
+        isPasswordMatching: false,
+        disabled: true,
+        canBeSubmitted: false,
     };
 
     handleEmailChange(event) {
-        this.setState({ email: event.target.value },
+        this.setState({ email: event.target.value, touched: true },
             () => {
                 console.log("IN HERE")
                 this.validateEmail();
             })
     }
 
-    handleUserNameChange(event){
-        this.setState({ username : event.target.value },
-        () =>{
-            this.isUsernameLengthZero();
-        })
+    handleUserNameChange(event) {
+        this.setState({ username: event.target.value, touched: true },
+            () => {
+                this.isUsernameLengthZero();
+            })
     }
 
-    handlePassword(event){
-        this.setState({ password : event.target.value },
-        () =>{
-            if(this.state.password.length === 0){
-                this.setState({ passwordLengthZero : true })
-            }
-            else{
-                this.setState({ passwordLengthZero : false })
-            }
+    handlePassword(event) {
+        this.setState({ password: event.target.value, touched: true },
+            () => {
+                if (this.state.password.length === 0) {
+                    this.setState({ passwordLengthZero: true })
+                }
+                else {
+                    this.setState({ passwordLengthZero: false })
+                }
 
-            this.comparePasswords()
-        })
+                this.comparePasswords()
+            })
     }
 
-    handleConfirmPassword(event){
-        this.setState({ confirmPassword : event.target.value },
-        () =>{
-            this.comparePasswords()
-        })
+    handleConfirmPassword(event) {
+        this.setState({ confirmPassword: event.target.value, touched: true },
+            () => {
+                this.comparePasswords()
+            })
     }
 
-    isUsernameLengthZero(){
-        if(this.state.username.length === 0){
-            this.setState({ usernameLengthZero : true })
+    isUsernameLengthZero() {
+        var alphaExp = /^[a-zA-Z]+$/;
+        if (!(this.state.username.length === 0 ) && (this.state.username.match(alphaExp))) {
+             this.setState({ usernameLengthZero: false },
+                () =>{
+                    this.allSet()
+                })
         }
-        else{
-            this.setState({ usernameLengthZero : false })
+        else {
+          
+                this.setState({ usernameLengthZero: true },
+                    () =>{
+                        this.allSet()
+                    })
         }
     }
 
-    comparePasswords(){
-        if(validator.equals(this.state.password, this.state.confirmPassword)){
-            this.setState({ isPasswordMatching : true })
+    comparePasswords() {
+        // validator.equals(this.state.password, this.state.confirmPassword)
+        if (validator.equals(this.state.password, this.state.confirmPassword)) {
+            console.log("same")
+            this.setState({ isPasswordMatching: true },
+                () =>{
+                    this.allSet()
+                })
         }
-        else{
-            this.setState({ isPasswordMatching : false })
+        else {
+            console.log("different")
+            this.setState({ isPasswordMatching: false },
+                () =>{
+                   this.allSet()
+                })
         }
     }
 
     validateEmail() {
-        if (validator.isEmail(this.state.email)) {
-            this.setState({ isInvalidEmail: false })
+        if ( /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test((this.state.email) ) ){
+            this.setState({ isInvalidEmail: false },
+                () =>{
+                    this.allSet()
+                })
         }
-        else{
-            this.setState({ isInvalidEmail: true })
+        else {
+            this.setState({ isInvalidEmail: true },
+                () =>{
+                    this.allSet()
+                })
         }
     }
 
 
     /* WHEN EVERYTHING IS RIGHT, EXECUTE THIS.... */
-    allSet(){
-        ( this.state.isInvalidEmail &&
-          this.state.isPasswordMatching ) ?
-          this.setState({ allSet : true }) :
-          this.setState({ allSet : false })
+    allSet() {
+           console.log("invalidEmail : " ,this.state.isInvalidEmail,
+           "passwordLengthZero : " , this.state.passwordLengthZero ,
+           "username:",this.state.usernameLengthZero,
+           "match? : ",this.state.isPasswordMatching);   
+           
+        if(!this.state.isInvalidEmail && !this.state.passwordLengthZero && !this.state.usernameLengthZero &&
+            this.state.isPasswordMatching) {
+                this.setState({ disabled: false },function() {
+                    console.log('callbacked ', this.state)
+
+                }) 
+            } 
+        else {
+            console.log("all matched else ", this.state);
+            this.setState({ disabled: true },function() {
+                console.log('callbacked ', this.state)
+                
+            }) 
+        }    
     }
 
     renderRedirect() {
@@ -130,8 +168,8 @@ class Signup extends Component {
     }
 
     handleSubmit(e) {
-        
-        if (!this.canBeSubmitted) {
+
+        if (!this.allSet) {
             e.preventDefault();
             return;
         }
@@ -187,7 +225,7 @@ class Signup extends Component {
                                     </Col>
                                     <Col md={9}>
                                         <FormControl onChange={this.handleEmailChange.bind(this)} name="email" type="email" placeholder="Email" value={this.state.email} />
-                                        { (this.state.isInvalidEmail) ? <div>Invalid email format!</div> : null}
+                                        {(this.state.isInvalidEmail && this.state.touched) ? <div>Invalid email format!</div> : null}
                                     </Col>
 
                                 </FormGroup>
@@ -196,8 +234,8 @@ class Signup extends Component {
                                         User Name
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl onChange = { this.handleUserNameChange.bind(this) } name="username" type="text" placeholder="Username" value={this.state.username} />
-                                        { (this.state.usernameLengthZero) ? <div>Set some username...</div> : null}
+                                        <FormControl onChange={this.handleUserNameChange.bind(this)} name="username" type="text" placeholder="Username" value={this.state.username} />
+                                        {(this.state.usernameLengthZero && this.state.touched) ? <div>Set some username...</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup controlId="password">
@@ -205,8 +243,8 @@ class Signup extends Component {
                                         Password
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl onChange = { this.handlePassword.bind(this) } name="password" type="password" placeholder="Password" value={this.state.password} />
-                                        { (this.state.passwordLengthZero) ? <div>Password cannot be empty...</div> : null}
+                                        <FormControl onChange={this.handlePassword.bind(this)} name="password" type="password" placeholder="Password" value={this.state.password} />
+                                        {(this.state.passwordLengthZero && this.state.touched) ? <div>Password cannot be empty...</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup controlId="confpsswd">
@@ -214,14 +252,15 @@ class Signup extends Component {
                                         Confirm Password
                                     </Col>
                                     <Col md={9}>
-                                        <FormControl onChange = { this.handleConfirmPassword.bind(this) } name="confpsswd" type="password" placeholder="Confirm Password" value={this.state.confpsswd} />
-                                        { (!this.state.isPasswordMatching) ? <div>Passwords donot match!</div> : null}
+                                        <FormControl onChange={this.handleConfirmPassword.bind(this)} name="confpsswd" type="password" placeholder="Confirm Password" value={this.state.confpsswd} />
+                                        {(!this.state.isPasswordMatching && this.state.touched) ? <div>Passwords donot match!</div> : null}
                                     </Col>
                                 </FormGroup>
                                 <FormGroup>
                                     <Col smOffset={3} mdOffset={3} sm={9} md={6}>
                                         {this.renderRedirect()}
-                                        <Button type="button" bsStyle="success" disabled = { !this.state.canBeSubmitted } ><i className="icon-hand-right"></i>Sign Up</Button>
+                                        <Button type="button" bsStyle="success"  disabled = {this.state.disabled} 
+                                        ><i className="icon-hand-right"></i>Sign Up</Button>
                                         <span style={{ marginLeft: 8 }}> OR </span>
                                         <Button type="submit" bsStyle="primary">Sign Up with Stackoverflow</Button>
                                     </Col>
